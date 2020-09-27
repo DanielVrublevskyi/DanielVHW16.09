f3b3cc4db79332cb5fbd5755ac749395acae61e1
DanielVrublevskyi <daniil24267@gmail.com>
2020-09-27 13:50:29 +0300

    27ю09

--- src/test/java/com/selenium/SuperFarm.java
+++ src/test/java/com/selenium/SuperFarm.java
@@ -25,10 +25,11 @@ public class SuperFarm {
 
         wd.navigate().to("https://shop.super-pharm.co.il/");
 //        wd.findElement(By.cssSelector("body.page-homepage.pageType-ContentPage.template-pages-layout-landingLayout2Page.pageLabel-homepage.language-he.INDlangdirRTL.INDpositionRight.INDDesktop.INDChrome:nth-child(2) div.site-wrap:nth-child(4) nav.navbar.navbar-top div.container-fluid div.row-fluid div.col-xs-6:nth-child(2) ul.nav.navbar-nav.nav-jusified li.yCmsComponent:nth-child(6) > a:nth-child(1)")).click();
-//        WebElement nav = wd.findElement(By.xpath("//nav[@id='navbar-header-top']"));
-//        nav.findElement(By.xpath(".//*[@title='תנאי שימוש']")).click();
 
-        wd.findElement(By.xpath("//nav[@id='navbar-header-top']//*[@title='תנאי שימוש']")).click();
+        WebElement nav = wd.findElement(By.xpath("//nav[@id='navbar-header-top']"));
+        nav.findElement(By.xpath(".//*[@title='תנאי שימוש']")).click();
+
+//        wd.findElement(By.xpath("//nav[@id='navbar-header-top']//*[@title='תנאי שימוש']")).click();
 
     }
 }
